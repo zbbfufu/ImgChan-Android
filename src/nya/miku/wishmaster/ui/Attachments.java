e0bd4ec4e5a0b470354cafed61c0f1575c38d91b
Anonymous <anonymous@example.com>
2019-09-26 11:09:53 +0300

    Do not use original name if it is only an extension

--- src/nya/miku/wishmaster/ui/Attachments.java
+++ src/nya/miku/wishmaster/ui/Attachments.java
@@ -90,7 +90,11 @@ public class Attachments {
      * @param attachment модель вложения
      */
     public static String getAttachmentDisplayName(AttachmentModel attachment) {
-        if (attachment.originalName != null && attachment.originalName.length() != 0) return attachment.originalName;
+        if ((attachment.originalName != null) &&
+            (attachment.originalName.length() != 0) &&
+            (!attachment.originalName.equals(getAttachmentExtention(attachment)))) {
+            return attachment.originalName;
+        }
         if (attachment.type == AttachmentModel.TYPE_OTHER_NOTFILE) {
             return attachment.path == null ? "" : attachment.path;
         }
