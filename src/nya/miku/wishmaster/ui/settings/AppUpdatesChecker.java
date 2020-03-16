0580ba720f052dab7d37d92d9c42595224251c51
Anonymous <anonymous@example.com>
2020-03-16 17:30:14 +0300

    Download and install update from application

--- src/nya/miku/wishmaster/ui/settings/AppUpdatesChecker.java
+++ src/nya/miku/wishmaster/ui/settings/AppUpdatesChecker.java
@@ -18,20 +18,28 @@
 
 package nya.miku.wishmaster.ui.settings;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import nya.miku.wishmaster.R;
 import nya.miku.wishmaster.api.interfaces.CancellableTask;
+import nya.miku.wishmaster.api.interfaces.ProgressListener;
 import nya.miku.wishmaster.common.Async;
+import nya.miku.wishmaster.common.IOUtils;
 import nya.miku.wishmaster.common.Logger;
 import nya.miku.wishmaster.common.MainApplication;
 import nya.miku.wishmaster.http.client.ExtendedHttpClient;
 import nya.miku.wishmaster.http.streamer.HttpRequestModel;
 import nya.miku.wishmaster.http.streamer.HttpStreamer;
 import nya.miku.wishmaster.lib.org_json.JSONObject;
+import nya.miku.wishmaster.lib.UriFileUtils;
 import nya.miku.wishmaster.ui.tabs.UrlHandler;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.widget.Toast;
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.HttpHeaders;
@@ -49,6 +57,7 @@ public class AppUpdatesChecker {
     }
 
     public static void checkForUpdates(final Activity activity, final boolean silent) {
+        final ExtendedHttpClient httpClient = new ExtendedHttpClient(null);
         final CancellableTask task = new CancellableTask.BaseCancellableTask();
         final ProgressDialog progressDialog;
         if (silent) {
@@ -71,7 +80,6 @@ public class AppUpdatesChecker {
                 JSONObject response;
                 try {
                     String url = URL_PATH + (MainApplication.getInstance().settings.isUpdateAllowBeta() ? URL_BETA : URL_STABLE);
-                    ExtendedHttpClient httpClient = new ExtendedHttpClient(null);
                     Header[] customHeaders = new Header[] { new BasicHeader(HttpHeaders.ACCEPT, "application/vnd.github.v3+json") };
                     HttpRequestModel request = HttpRequestModel.builder().setGET().setCustomHeaders(customHeaders).build();
                     response = HttpStreamer.getInstance().getJSONObjectFromUrl(url, request, httpClient, null, task, false);
@@ -94,15 +102,15 @@ public class AppUpdatesChecker {
                         }
                         try {
                             if (result == null) throw new Exception();
-                            String newVersionName = result.getString("name");
-                            String currentVersionName = activity.getPackageManager().getPackageInfo(activity.getPackageName(), 0).versionName;
+                            final String newVersionName = result.getString("name");
+                            final String currentVersionName = activity.getPackageManager().getPackageInfo(activity.getPackageName(), 0).versionName;
                             if (!currentVersionName.equals(newVersionName)) {
                                 String newVersionInfo = result.getString("body");
                                 final String url = result.getJSONArray("assets").getJSONObject(0).getString("browser_download_url");
                                 DialogInterface.OnClickListener onClickYes = new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int which) {
-                                        UrlHandler.launchExternalBrowser(activity, url);
+                                        install(activity, url, newVersionName);
                                     }
                                 };
                                 new AlertDialog.Builder(activity).
@@ -117,6 +125,80 @@ public class AppUpdatesChecker {
                         } catch (Exception e) {
                             Logger.e(TAG, e);
                             if (!silent) Toast.makeText(activity, R.string.app_update_update_error, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+            private void install(final Activity activity, final String url, final String version) {
+                final CancellableTask task = new CancellableTask.BaseCancellableTask();
+                final ProgressDialog progressDialog = new ProgressDialog(activity);
+                final String name = "Overchan-v" + version + ".apk";
+                progressDialog.setMessage(name);
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        task.cancel();
+                    }
+                });
+                final ProgressListener listener = new ProgressListener() {
+                    @Override
+                    public void setProgress(long value) {
+                        progressDialog.setProgress((int)value);
+                    }
+                    @Override
+                    public void setMaxValue(long value) {
+                        progressDialog.setMax((int)value);
+                    }
+                    @Override
+                    public void setIndeterminate() {
+                        progressDialog.setIndeterminate(true);
+                    }
+                };
+                progressDialog.show();
+                Async.runAsync(new Runnable() {
+                    @Override
+                    public void run() {
+                        FileOutputStream out = null;
+                        try {
+                            File file = new File(MainApplication.getInstance().settings.getDownloadDirectory(), name);
+                            out = new FileOutputStream(file);
+                            HttpRequestModel request = HttpRequestModel.builder().setGET().build();
+                            HttpStreamer.getInstance().downloadFileFromUrl(url, out, request, httpClient, listener, task, false);
+                            try {
+                                Intent intent;
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                                    intent = new Intent(Intent.ACTION_INSTALL_PACKAGE);
+                                    intent.setData(UriFileUtils.getContentUri(activity, file));
+                                } else {
+                                    intent = new Intent(Intent.ACTION_VIEW);
+                                    intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+                                }
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                activity.startActivity(intent);
+                            } catch (final Exception e) {
+                                Logger.e(TAG, e);
+                                Async.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(activity, activity.getString(R.string.app_update_install_error) +
+                                                ":\n" + e.getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
+                        } catch (final Exception e) {
+                            Logger.e(TAG, e);
+                            Async.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(activity, activity.getString(R.string.app_update_download_error) +
+                                            ":\n" + e.getMessage(), Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        } finally {
+                            IOUtils.closeQuietly(out);
+                            progressDialog.dismiss();
                         }
                     }
                 });
