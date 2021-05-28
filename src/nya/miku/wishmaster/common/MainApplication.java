67b2f45774d89e3c77ca8a06efdbb0cdbac03831
Eilhart <eilhart@users.noreply.github.com>
2021-05-28 21:49:28 +0600

    Remove lolifox from boards list

--- src/nya/miku/wishmaster/common/MainApplication.java
+++ src/nya/miku/wishmaster/common/MainApplication.java
@@ -108,7 +108,6 @@ public class MainApplication extends Application {
             "nya.miku.wishmaster.chans.sushichan.SushichanModule",
             "nya.miku.wishmaster.chans.tumbach.TumbachModule",
             "nya.miku.wishmaster.chans.wizchan.WizchanModule",
-            "nya.miku.wishmaster.chans.infinity.LolifoxModule",
             "nya.miku.wishmaster.chans.lampach.LampachModule",
             "nya.miku.wishmaster.chans.kropyvach.KropyvachModule",
             "nya.miku.wishmaster.chans.hispachan.HispachanModule",
