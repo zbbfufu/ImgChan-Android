ea9a78251c8b96d91b32c553ad3e468c3a41d908
Eilhart <eilhart@users.noreply.github.com>
2021-07-07 19:39:34 +0600

    Cirno: add more stormwall checks

--- src/nya/miku/wishmaster/chans/cirno/CirnoModule.java
+++ src/nya/miku/wishmaster/chans/cirno/CirnoModule.java
@@ -233,7 +233,7 @@ public class CirnoModule extends StormwallChanModule {
 
     @Override
     public CaptchaModel getNewCaptcha(String boardName, String threadNumber, ProgressListener listener, CancellableTask task) throws Exception {
-        String captchaUrl = getUsingUrl() + "/cgi-bin/captcha" + (boardName.equals("b") || boardName.equals("a") ? "1" : "") + ".pl/" +
+        String captchaUrl = getUsingUrl() + "cgi-bin/captcha" + (boardName.equals("b") || boardName.equals("a") ? "1" : "") + ".pl/" +
                 boardName + "/?key=" + (threadNumber == null ? "mainpage" : ("res" + threadNumber));
         return downloadCaptcha(captchaUrl, listener, task);        
     }
@@ -281,6 +281,7 @@ public class CirnoModule extends StormwallChanModule {
                     }
                 }
             } else if (response.statusCode == 200) {
+                checkForStormwall(url, response);
                 ByteArrayOutputStream output = new ByteArrayOutputStream(1024);
                 IOUtils.copyStream(response.stream, output);
                 String htmlResponse = output.toString("UTF-8");
@@ -323,6 +324,7 @@ public class CirnoModule extends StormwallChanModule {
         try {
             response = HttpStreamer.getInstance().getFromUrl(url, request, httpClient, null, task);
             if (response.statusCode == 200) {
+                checkForStormwall(url, response);
                 ByteArrayOutputStream output = new ByteArrayOutputStream(1024);
                 IOUtils.copyStream(response.stream, output);
                 String htmlResponse = output.toString("UTF-8");
